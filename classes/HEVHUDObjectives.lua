ee632631b1b9da25bc004b8811dac7cfe8a7058e
offyerrocker <offyerrocker88@gmail.com>
2025-12-02 18:04:57 -0800

    objectives panel animation
    left-aligned objectives panel grow animation
    objective amount text now animates with static time instead of character-based, as it's too short otherwise

--- classes/HEVHUDObjectives.lua
+++ classes/HEVHUDObjectives.lua
@@ -31,17 +31,16 @@ function HEVHUDObjectives:setup()
 		name = "objective",
 		x = vars.OBJECTIVES_X,
 		y = vars.OBJECTIVES_Y,
-		w = vars.OBJECTIVES_W,
+		w = 1, --vars.OBJECTIVES_W,
 		h = vars.OBJECTIVES_H,
 		alpha = 1,
 		layer = 3,
 		visible = false
 	})
 	
-	local BG_BOX_ALPHA = self._config.General.BG_BOX_ALPHA
 	self._BG_BOX_COLOR = HEVHUD.colordecimal_to_color(self._config.General.BG_BOX_COLOR)
 	
-	self.CreateBGBox(self._objective,nil,nil,{alpha=BG_BOX_ALPHA,valign="grow",halign="grow",layer=1},{color=self._BG_BOX_COLOR})
+	self.CreateBGBox(self._objective,nil,nil,{alpha=self._config.General.BG_BOX_ALPHA,valign="grow",halign="grow",layer=1},{color=self._BG_BOX_COLOR})
 	
 	self._objective:text({
 		name = "text",
@@ -57,7 +56,7 @@ function HEVHUDObjectives:setup()
 		color = self._objective_text_color,
 		valign = "grow",
 		halign = "grow",
-		wrap = true,
+--		wrap = true, -- word wrap does not seem to work with custom fonts; it reports a negative line height, causing 
 		layer = 4
 	})
 	self._objective:text({
@@ -76,14 +75,43 @@ function HEVHUDObjectives:setup()
 		halign = "grow",
 		layer = 4
 	})
+	
 end
 
 function HEVHUDObjectives:activate_objective(data)
 --	Print("activate")
 --	logall(data)
 	
-	self._objective:show()
+	local cb_done
+	
+	if not self._objective:visible() then
+		cb_done = callback(self,self,"_activate_objective",data)
+	end
+	
 	self._active_objective_id = data.id
+
+	local vars = self._config.Objectives
+	local objective_w = vars.OBJECTIVES_W
+	if data.text then
+		local objective_text = self._objective:child("text")
+		objective_text:set_text(data.text)
+		local tx,ty,tw,th = objective_text:text_rect()
+		objective_w = vars.OBJECTIVE_LABEL_X + tw + vars.OBJECTIVE_LABEL_HOR_MARGIN
+	end
+	
+	self._objective:show()
+	self._objective:stop()
+	self._objective:animate(AnimateLibrary.animate_grow_w_left,cb_done,vars.ANIM_OBJECTIVE_PANEL_DURATION,nil,objective_w)
+	
+	if cb_done then
+		return
+	else
+		self:_activate_objective(data)
+	end
+	
+end
+
+function HEVHUDObjectives:_activate_objective(data)
 	local objective_text = self._objective:child("text")
 	objective_text:stop()
 	objective_text:animate(AnimateLibrary.animate_text_mission,nil,data.text,nil,self._objective_text_color,self._objective_flash_color,nil)
@@ -99,23 +127,25 @@ end
 function HEVHUDObjectives:remind_objective(id)
 --	Print("remind",id)
 	
+	-- flash the objective bgbox twice
+	
 end
 
 function HEVHUDObjectives:complete_objective(data)
 	if data.id == self._active_objective_id then
 --		Print("complete")
 --		logall(data)
-		
 	end
 end
 
 function HEVHUDObjectives:update_amount_objective(data)
 --	Print("Update amount")
 --	logall(data)
+	local vars = self._config.Objectives
 	local objective_amount = self._objective:child("amount")
 	objective_amount:show()
 	objective_amount:stop()
-	objective_amount:animate(AnimateLibrary.animate_wait,0.5,AnimateLibrary.animate_text_mission,nil,string.format("%i/%i",data.current_amount or 0,data.amount),nil,self._objective_text_color,self._objective_flash_color,nil)
+	objective_amount:animate(AnimateLibrary.animate_wait,vars.ANIM_OBJECTIVE_AMOUNT_ACTIVATE_DELAY,AnimateLibrary.animate_text_mission,nil,string.format("%i/%i",data.current_amount or 0,data.amount),vars.ANIM_OBJECTIVE_AMOUNT_UPDATE_DURATION,self._objective_text_color,self._objective_flash_color,nil)
 end
 
 
