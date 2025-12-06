0745a03098483632cc08d84f5574ee2725f34437
offyerrocker <offyerrocker88@gmail.com>
2025-12-06 01:32:10 -0800

    fixed ammo pickup detection
    underbarrel ammo pickup is now calculated correctly
    ammo pickup amount now only calculates actual ammo pickup- does not include overflow/"stored ammo"
    all underbarrels now use the placeholder contraband_m203 weapon silhouette in the ammo pickup

--- hooks/raycastweaponbase.lua
+++ hooks/raycastweaponbase.lua
@@ -1,84 +1,54 @@
 --local SHOW_UNDERBARREL_AFTER_MAIN_AMMO = false
 local DETECT_UNDERBARREL_AMMO = true
 
-
-local underbarrel_ammo_amount = 0
+local main_ammo_amount = 0
+local underbarrel_ammo_amount = {}
 if DETECT_UNDERBARREL_AMMO then
 	-- this is a stupid ass way to detect underbarrel ammo but the vanilla pd2 func doesn't return ammo data for underbarrels 
 	Hooks:PreHook(RaycastWeaponBase,"add_ammo","hevhud_raycastweaponbase_addammo_pre",function(self, ratio, add_amount_override)
-		underbarrel_ammo_amount = 0
+		main_ammo_amount = self:get_ammo_total()
 		for i,gadget in pairs(self:get_all_override_weapon_gadgets()) do 
 			if gadget and gadget.ammo_base then
-				underbarrel_ammo_amount = gadget:ammo_base():get_ammo_total()
-				break
+				underbarrel_ammo_amount[i] = gadget:ammo_base():get_ammo_total()
 			end
 		end
 	end)
 end
 
 Hooks:PostHook(RaycastWeaponBase,"add_ammo","hevhud_raycastweaponbase_addammo_post",function(self, ratio, add_amount_override)
-	local picked_up,add_amount = Hooks:GetReturn()
-	
+	local picked_up,_ = Hooks:GetReturn()
 	if picked_up then
-		local slot = self:selection_index()
+	
+		-- main weapon pickup
+		if main_ammo_amount then
+			local main_ammo = self:get_ammo_total() - main_ammo_amount
+			
+			if main_ammo > 0 then
+				local weapon_id = self._name_id
+				local wtd = tweak_data.weapon[weapon_id] or tweak_data.weapon.amcar
+				HEVHUD:ShowAmmoPickup(wtd.use_data.selection_index,weapon_id,main_ammo,nil,nil,nil)
+			end
+			main_ammo_amount = nil
+		end
 		
+		-- underbarrel pickup
 		if DETECT_UNDERBARREL_AMMO then
 			for i,gadget in pairs(self:get_all_override_weapon_gadgets()) do 
 				if gadget and gadget.ammo_base then
-					local underbarrel_add_amount = gadget:ammo_base():get_ammo_total() - underbarrel_ammo_amount
-					if underbarrel_add_amount >= 1 then
-						-- it looks like vanilla pd2 adds the underbarrel ammo added to main ammo added when reporting ammo picked up,
-						-- but this doesn't work- reports a negative value
-						--add_amount = add_amount - underbarrel_add_amount 
-						local override_weapon_icon,override_weapon_rect = "guis/textures/pd2/blackmarket/icons/weapons/outline/contraband_m203",nil
-						HEVHUD:ShowAmmoPickup(slot + 2,gadget.name_id,underbarrel_add_amount,nil,override_weapon_icon,override_weapon_rect)
-					end
-					break
-				end
-			end
-		end
-		
-		if add_amount >= 1 then -- todo show decimal setting
-			local weapon_id = self:get_name_id()
-			HEVHUD:ShowAmmoPickup(slot,weapon_id,add_amount,HEVHUD._font_icons[HEVHUD.GetHL2WeaponIcons(weapon_id) or ""],nil,nil,nil)
-		end
-		
-		
-		--[[
-		local underbarrel_ammo = {}
-		local override_weapon_icon,override_weapon_rect = "guis/textures/pd2/blackmarket/icons/weapons/outline/contraband_m203",nil
-		for i,gadget in pairs(self:get_all_override_weapon_gadgets()) do 
-		
-			if gadget and gadget.ammo_base then
-				local ammo_base = gadget:ammo_base()
-				underbarrel_ammo[i] = {
-					gadget = gadget,
-					ammo_base = ammo_base,
-					amount = ammo_base:get_ammo_total()
-				}
-					
-				local underbarrel_add_amount = ammo_base:get_ammo_total() - data.amount
-				if underbarrel_add_amount > 0 then
-					add_amount = add_amount - underbarrel_add_amount
-					data.add_amount = add_amount
-					
-					if not SHOW_UNDERBARREL_AFTER_MAIN_AMMO then
-						HEVHUD:ShowAmmoPickup(slot + 2,gadget.name_id,add_amount,nil,override_weapon_icon,override_weapon_rect)
+					if underbarrel_ammo_amount[i] then
+						local underbarrel_ammo = gadget:ammo_base():get_ammo_total() - underbarrel_ammo_amount[i]
+						underbarrel_ammo_amount[i] = nil
+						if underbarrel_ammo >= 1 then
+							local weapon_id = gadget.name_id
+							local wtd = tweak_data.weapon[weapon_id]
+							if wtd then
+								local override_weapon_icon,override_weapon_rect = "guis/textures/pd2/blackmarket/icons/weapons/outline/contraband_m203",nil
+								HEVHUD:ShowAmmoPickup(wtd.use_data.selection_index,weapon_id,underbarrel_ammo,nil,override_weapon_icon,override_weapon_rect)
+							end
+						end
 					end
 				end
 			end
 		end
-		
-		local weapon_id = self:get_name_id()
-		HEVHUD:ShowAmmoPickup(slot,weapon_id,add_amount,HEVHUD._font_icons[HEVHUD.GetHL2WeaponIcons(weapon_id) or ""],nil,nil,nil)
-		if SHOW_UNDERBARREL_AFTER_MAIN_AMMO then
-			-- it's technically slightly less efficient to do this,
-			-- but also potentially annoying if players wish to see their main weapon pickup first
-			local override_weapon_icon,override_weapon_rect = "guis/textures/pd2/blackmarket/icons/weapons/outline/contraband_m203",nil
-			for i,data in pairs(underbarrel_ammo) do 
-				HEVHUD:ShowAmmoPickup(slot + 2,gadget.name_id,data.add_amount,nil,override_weapon_icon,override_weapon_rect)
-			end
-		end
-	--]]
 	end
 end)
